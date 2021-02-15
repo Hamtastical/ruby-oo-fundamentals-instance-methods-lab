dfdd05a477af01aaeeee3a8e3ce1b8a0f2849ecd
meili <meiliham1@gmail.com>
2021-02-15 13:40:46 -0500

    commit tree 0352df1dd96004950c4f679adde343c9821d1a74

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog #make Dog class
+    def bark #def method for dog barking
+        puts "Woof!" #puts out the command
+        
+    def sit #def method to sit
+        puts "The Dog is sitting" #puts out the string
+    end
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person #make a class for Person
+    def talk #def method to talk within the class
+        puts "Hello World!" # puts out string
+    def walk #def method for walking within the class
+        puts "The Person is walking" #string for the person is walking
+    end
+end
+end
